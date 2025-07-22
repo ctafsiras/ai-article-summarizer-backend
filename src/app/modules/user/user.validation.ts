3b1a1613730c5bebbfacc386829d90443e7d838a
Chowdhury Tafsir Ahmed Siddiki <ctafsiras@gmail.com>
2025-07-22 23:37:55 +0600

    auth fix

--- src/app/modules/user/user.validation.ts
+++ src/app/modules/user/user.validation.ts
@@ -3,33 +3,21 @@ import { z } from 'zod';
 const createUserValidationSchema = z.object({
   body: z.object({
     email: z.string().email(),
-    username: z.string(),
+    name: z.string(),
     password: z.string(),
     role: z.string().optional(),
-    firstName: z.string(),
-    lastName: z.string(),
-    profilePicture: z.string().optional(),
-  })
+  }),
 });
 
 const updateUserValidationSchema = z.object({
   body: z.object({
-    firstName: z.string().optional(),
-    lastName: z.string().optional(),
-    profilePicture: z.string().optional(),
-  })
+    name: z.string().optional(),
+    password: z.string().optional(),
+    role: z.string().optional(),
+  }),
 });
-
-const updateUserAccessSchema = z.object({
-  body: z.object({
-    role: z.enum(['manager', 'moderator']).optional(),
-    isBlocked: z.boolean().optional(),
-    isDeleted: z.boolean().optional(),
-  })
-})
 
 export const UserValidation = {
   createUserValidationSchema,
   updateUserValidationSchema,
-  updateUserAccessSchema,
 };
