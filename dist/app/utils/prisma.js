8df331cfca2e7eea9e16f584027a847eec74777b
Chowdhury Tafsir Ahmed Siddiki <ctafsiras@gmail.com>
2025-07-23 21:56:33 +0600

    prima

--- dist/app/utils/prisma.js
+++ dist/app/utils/prisma.js
@@ -2,6 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.prisma = void 0;
 const client_1 = require("@prisma/client");
+// globalForPrisma is used to store the PrismaClient instance in the global scope
 const globalForPrisma = globalThis;
 exports.prisma = globalForPrisma.prisma || new client_1.PrismaClient();
 if (process.env.NODE_ENV !== 'production')
--- src/app/utils/prisma.ts
+++ src/app/utils/prisma.ts
@@ -1,5 +1,6 @@
 import { PrismaClient } from '@prisma/client';
 
+// globalForPrisma is used to store the PrismaClient instance in the global scope
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
 
 export const prisma = globalForPrisma.prisma || new PrismaClient();
