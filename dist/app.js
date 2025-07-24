544928d0ab2b28a54c7abfd190aceea29042fdb2
Chowdhury Tafsir Ahmed Siddiki <ctafsiras@gmail.com>
2025-07-24 14:58:24 +0600

    updated

--- dist/app.js
+++ dist/app.js
@@ -1,12 +1,9 @@
 "use strict";
+/* eslint-disable no-unused-vars */
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-/* eslint-disable no-undef */
-/* eslint-disable no-unused-vars */
-/* eslint-disable @typescript-eslint/no-unused-vars */
-/* eslint-disable @typescript-eslint/no-explicit-any */
 const cors_1 = __importDefault(require("cors"));
 const express_1 = __importDefault(require("express"));
 const globalErrorhandler_1 = __importDefault(require("./app/middlewares/globalErrorhandler"));
